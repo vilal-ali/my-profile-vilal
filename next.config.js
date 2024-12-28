cb041e1cec73f9009493641d91d60160e42bd090
vilalali <vilal.ali@ihub-data.iiit.ac.in>
2024-12-29 02:53:15 +0530

    updted

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   // Other configurations (if any)
-  distDir: 'build', // Custom output directory
+  distDir: 'out', // Custom output directory
   eslint: {
     // Your ESLint configuration options
     ignoreDuringBuilds: true, // Example option
